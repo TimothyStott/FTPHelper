43a1f265a3d62ded41435e4317e78c326bf17191
Tim <timothy.b.stott@gmail.com>
2022-09-02 15:58:13 -0400

    Add ftp connection test and helper docs

---
+++ FTPHelper/FTPclasses/FtpMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using FluentFTP;
+
+namespace FTPHelper.FTPclasses
+{
+    public static class FtpMethods
+    {
+        public static bool TestConnection(string ipAddress, string user, string pass)
+        {
+            try
+            {
+                var conn = new FtpClient("ftp.dlptest.com/", "dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu");
+                conn.Connect();
+                MessageBox.Show("FTP Connection Successful", "Success");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;                
+            }
+
+        }
+
+    }
+}
--- FTPHelper/MainWindow.xaml.cs
+++ FTPHelper/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FTPHelper.FTPclasses;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -25,6 +27,75 @@ namespace FTPHelper
         {
             InitializeComponent();   
             
+        }
+
+        private void testConnectionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (FtpMethods.TestConnection(@"ftp://ftp.dlptest.com", "dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu"))
+            {
+                openConnectionButton.Visibility = Visibility.Visible;
+                openConnectionButton.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Connection Failed", "Error");
+            }
+        }
+
+        private void openPDF_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (Process pdfOpen = new Process())
+                {
+                    pdfOpen.StartInfo.UseShellExecute = true;
+                    pdfOpen.StartInfo.FileName = "..\\..\\..\\Documents\\setIpHandpad.pdf";
+                    pdfOpen.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("File not found call Tech Support");
+            }
+        }
+
+        private void openGoogle_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (Process openGoogle = new Process())
+                {
+                    openGoogle.StartInfo.UseShellExecute = true;
+                    openGoogle.StartInfo.FileName = @"https://letmegooglethat.com/?q=Set+IPv4+IP+address";
+                    openGoogle.Start();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something went wrong. Please just google it.");
+            }
+
+
+        }
+
+        private void openConnectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (Process openExplorer = new Process())
+                {
+
+                    System.Diagnostics.Process.Start("explorer", @"ftp://ftp.dlptest.com")  ;
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something went wrong. Please call tech support");
+            }
+
+
         }
     }
 }
